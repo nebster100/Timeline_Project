ffb04ecee959132855ab5c116e8272502bffba77
Ben Eisenberg <nebster100@berkeley.edu>
2016-06-30 14:11:30 -0400

    V 0.0.05 initialize repo

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/about', function (req, res){
+	res.send('About us');
+});
+
+app.use(express.static(__dirname + '/'));
+
+app.listen(port, function() {
+	console.log('Server Started at port ' + port);
+});
