6e6541b35a740d71a4967de4692ff2c01f97eed5
Ben Eisenberg <nebster100@berkeley.edu>
2016-07-01 13:37:52 -0400

    T

--- server.js
+++ server.js
@@ -7,6 +7,31 @@
 app.use(express.static(__dirname + '/'));
 app.use(bodyParser.json());
  
+app.get('/timeline', function (req,res){
+	db.timelineObject.findAll().then(function (tlObjects){
+		var uniqueTLObjects = [];
+		tlObjects.forEach(function (tlObject) {
+			if(isUnique(tlObject, uniqueTLObjects)){
+				uniqueTLObjects.push(tlObject);
+			}
+		});
+		res.status(200).json(uniqueTLObjects);
+	}, function (e){
+		res.status(400).json(e);
+	});
+});
+
+function isUnique(tlObj, tlArr){
+	var unique = true;
+	tlArr.forEach(function (tlTest){
+		if(tlTest.timelineName === tlObj.timelineName){
+			unique =  false;
+		}
+	});
+	return unique;
+}
+
+
  app.listen(port, function() {
  	console.log('Server Started at port ' + port);
  });
