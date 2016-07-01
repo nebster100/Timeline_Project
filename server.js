a9ab6822fbbc4652b368fa55e423edd01d7a4a12
Ben Eisenberg <nebster100@berkeley.edu>
2016-07-01 13:32:06 -0400

    T

--- server.js
+++ server.js
@@ -2,11 +2,8 @@
  var app = express();
  var port = process.env.PORT || 3000;
  
- app.get('/about', function (req, res){
- 	res.send('About us');
- });
- 
  app.use(express.static(__dirname + '/'));
+ app.use(bodyParser.json());
  
  app.listen(port, function() {
  	console.log('Server Started at port ' + port);
