662247f74dd7025598436321d02ab3e4b60b96db
Ben Eisenberg <nebster100@berkeley.edu>
2016-07-01 13:36:41 -0400

    T

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
  var express = require('express');
+ var bodyParser = require('body-parser');
+
  var app = express();
  var port = process.env.PORT || 3000;
  
- //app.use(express.static(__dirname + '/'));
- app.use(bodyParser.json());
+app.use(express.static(__dirname + '/'));
+app.use(bodyParser.json());
  
  app.listen(port, function() {
  	console.log('Server Started at port ' + port);
