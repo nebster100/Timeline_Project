7c43957c4d775b41773ea327f4813661895d1be1
Ben Eisenberg <nebster100@berkeley.edu>
2016-07-01 14:13:48 -0400

    T

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
-var db = require('./js/dataBase.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-//app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/'));
 app.use(bodyParser.json());
 
 app.get('/timeline', function (req,res){
@@ -60,8 +59,6 @@ app.post('/timeline', function (req,res){
 	});
 });
 
-db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
-});
