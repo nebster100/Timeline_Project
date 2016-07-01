e01606870f4eff6ad15c4dd31b45e727d0188a85
Ben Eisenberg <nebster100@berkeley.edu>
2016-07-01 14:14:29 -0400

    T

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./js/dataBase.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
