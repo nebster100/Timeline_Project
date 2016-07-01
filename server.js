933cf6360106f062d0d662a7c26b8a2d3aa5b0d3
Ben Eisenberg <nebster100@berkeley.edu>
2016-07-01 11:47:46 -0400

    Added JSON functionality/sourcing to the timeline. Currently only POST and GET

--- server.js
+++ server.js
@@ -1,13 +1,48 @@
 var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+var db = require('./js/dataBase.js');
 
-app.get('/about', function (req, res){
-	res.send('About us');
-});
+var app = express();
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/'));
+app.use(bodyParser.json());
 
-app.listen(port, function() {
-	console.log('Server Started at port ' + port);
+app.get('/timeline', function (req,res){
+	res.status(500).send();
+});
+
+app.get('/timeline/:name', function(req, res) {
+	console.log("hi");
+	var where = {
+		timelineName: req.params.name
+	}
+
+	db.timelineObject.findAll({
+		where: where
+	}).then(function (tlObjects){
+		res.json(tlObjects);
+	}, function(e){
+		res.status(400).json(e);
+	});
+});
+
+app.post('/timeline', function (req,res){
+	var body = _.pick(req.body, 'timelineName', 'year', 'description', 'url', 'img');
+	
+	db.timelineObject.create(body).then(function (tlObject){
+		res.json(tlObject.toJSON());
+	}, function (e){
+		res.status(400).json(e);
+	},
+	function() {
+		res.status(500).send();
+	});
+});
+
+db.sequelize.sync().then(function() {//Remember to remove force true
+	app.listen(PORT, function() {
+		console.log('Express listening on port ' + PORT + '!');
+	});
 });
