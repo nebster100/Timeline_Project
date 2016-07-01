0b5aaada226a93c8b4e2db39ad1b44c5c8e34f36
Ben Eisenberg <nebster100@berkeley.edu>
2016-07-01 13:51:05 -0400

    T

--- server.js
+++ server.js
@@ -8,31 +8,6 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/'));
 app.use(bodyParser.json());
- 
-app.get('/timeline', function (req,res){
-	db.timelineObject.findAll().then(function (tlObjects){
-		var uniqueTLObjects = [];
-		tlObjects.forEach(function (tlObject) {
-			if(isUnique(tlObject, uniqueTLObjects)){
-				uniqueTLObjects.push(tlObject);
-			}
-		});
-		res.status(200).json(uniqueTLObjects);
-	}, function (e){
-		res.status(400).json(e);
-	});
-});
-
-function isUnique(tlObj, tlArr){
-	var unique = true;
-	tlArr.forEach(function (tlTest){
-		if(tlTest.timelineName === tlObj.timelineName){
-			unique =  false;
-		}
-	});
-	return unique;
-}
-
 
 db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
